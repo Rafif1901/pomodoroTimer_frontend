91b59236df4f9d5c737bb359dba104521d422408
Rafif1901 <mrafifbatubara@gmail.com>
2026-05-03 19:16:47 +0700

    changing skip button color

--- app/page.tsx
+++ app/page.tsx
@@ -155,8 +155,8 @@ export default function MainPage() {
                             {isActive ? "PAUSE" : "START"}
                         </button>
                         {isActive && (
-                            <button onClick={() => setTimeLeft(0)} className="text-white hover:text-gray-200 transition">
-                                <svg className="w-10 h-10" fill="currentColor" viewBox="0 0 20 20"><path d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" /></svg>
+                            <button onClick={() => setTimeLeft(0)} className="bg-white text-black hover:text-gray-200 transition">
+                                <svg className="w-7 h-7 ml-1" fill="currentColor" viewBox="0 0 20 20"><path d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" /></svg>
                             </button>
                         )}
                     </div>
